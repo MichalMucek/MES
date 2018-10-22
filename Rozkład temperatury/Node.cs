c5f04e029d891f5117cb219d05d441f7a4ac61be
Michał Mucek <michal.mucek@outlook.com>
2018-10-23 01:42:26 +0200

    Initial commit

---
+++ Rozkład temperatury/Node.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Rozkład_temperatury
+{
+    public class Node
+    {
+        [JsonProperty("x")]
+        public double x { get; set; }
+        [JsonProperty("y")]
+        public double y { get; set; }
+        [JsonProperty("t0")]
+        public double t0 { get; set; }
+
+        /*public Node(string jsonFile)
+        {
+            JObject jObject = JObject.Parse(File.ReadAllText(jsonFile));
+            JToken jNode = jObject["node"];
+            x = (double)jNode["x"];
+            y = (double)jNode["y"];
+            t0 = (double)jNode["t0"];
+        }
+
+        public Node()
+        {
+
+        }*/
+
+        public override string ToString()
+        {
+            return $@"x: {x} | y: {y} | t0: {t0}";
+        }
+    }
+}
---
+++ Rozkład temperatury/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Rozkład_temperatury
+{
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberFormatInfo numberFormatInfo = new NumberFormatInfo();
+            numberFormatInfo.NumberDecimalSeparator = ".";
+            
+            //Node node = new Node(@"..\..\node.json");
+            JObject jObject = JObject.Parse(File.ReadAllText(@"..\..\node.json"));
+            JArray jArray = (JArray) jObject["node"];
+            List<Node> nodes = jArray.ToObject<List<Node>>();
+
+            JObject jObject2 = JObject.Parse(File.ReadAllText(@"..\..\node2.json"));
+            Node node2 = jObject2.ToObject<Node>();
+
+            double a = 1.3;
+            Console.WriteLine(a.ToString(numberFormatInfo));
+
+            foreach (var node in nodes)
+            {
+                Console.WriteLine(node.ToString());
+            }
+            Console.WriteLine(node2);
+            Console.ReadKey();
+        }
+    }
+}
