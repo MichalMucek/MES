317cf50784442fee4fa548707cc475093f9c659c
Michał Mucek <michal.mucek@gmail.com>
2018-12-03 03:26:36 +0100

    [TEST GAŁĘZI] Usunięcie zbędnego zakomentowania

--- MES_CP/Program.cs
+++ MES_CP/Program.cs
@@ -16,18 +16,6 @@ namespace MES_CP
                 Console.ReadKey();
                 Console.Clear();
             }
-
-            /*JObject ksiEtaBcJObject = JObject.Parse(File.ReadAllText(@"..\..\data\ksi_eta_BC.json"));
-            JArray surfacesJArray = (JArray)ksiEtaBcJObject["Surface"];
-            /*JObject[] ksiEtaJObject =
-            {
-                surfacesJArray[0],
-                surfacesJArray[1],
-                surfacesJArray[2],
-                surfacesJArray[3],
-            };#1#
-            Console.WriteLine(surfacesJArray[0]["ksi"].ToString());
-            Console.ReadKey();*/
         }
     }
 }
